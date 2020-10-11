c8c3d183f43bcd3b575ce2f53bd5a484751a5b9d
usernameSplash <kkfenix3085@naver.com>
2020-10-11 16:56:55 +0900

    Feat : Scrolling Background 1

---
+++ RunningYoumu/Assets/Scripts/Controller/BackgroundController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BackgroundController : MonoBehaviour
+{
+    MeshRenderer[] Quads;
+    float[] Speeds = { 0.05f, 0.1f, 0.2f, 0.4f };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Quads = gameObject.GetComponentsInChildren<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < Quads.Length; i++)
+        {
+            Quads[i].material.mainTextureOffset += new Vector2(Speeds[i] * Time.deltaTime, 0.0f);
+        }
+    }
+}
